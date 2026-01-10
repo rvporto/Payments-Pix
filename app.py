8fbc73e47b81040121063577f94bd973cc2ad664
rvporto <victorporto98@hotmail.com>
2026-01-10 17:17:52 -0300

    segunda atualização

--- app.py
+++ app.py
@@ -44,6 +44,25 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+    data = request.get_json()
+
+
+    #validações
+    if 'bank_payment_id' not in data and 'value' not in data:
+        return jsonify({"error": "Invalid payment data"}), 400
+    
+    #payment?
+    payment = Payment.query.filter_by(bank_payment_id=data.get('bank_payment_id')).first()
+    
+    if not payment or payment.paid:
+        return jsonify({"error": "Payment not found"}), 404
+    
+    if data.get("value") != payment.value:
+        return jsonify({"error": "Invalid payment value"}), 400
+    
+    payment.paid = True
+    db.session.commit()
+
     return jsonify({"message": "Pix payment created and confirmed"}), 201
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
